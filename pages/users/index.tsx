0bbd7d1bb90d87b2e46ea13574fd9c3453f354f7
Shivani Sharma <shivanisharma1941995@gmail.com>
2025-01-30 01:14:16 +0530

    usual fetching data CSR

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,6 +1,30 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import Link from "next/link"
+import useSWR from "swr"
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
 const Users = () => {
+
+  const { data, error } = useSWR('https://dummyjson.com/users', fetcher)
+
+  if(error) 
+    return <h2> Error Page</h2>
+
+  if(!data)
+    return <h2> Loading...</h2>
+
   return (
+    <>
     <h1>This is user home page</h1>
+    <h4> Users </h4>
+    { // @ts-expect-error
+     data && data.users && data.users.map((user) => (
+      <Link href={`users/${user.id}`} key={user.id}>
+     <li  style={{ color : 'yellowgreen'}}> {user.firstName}</li>
+     </Link>
+    ))}
+    </>
   )
 
 
