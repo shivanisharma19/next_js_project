45bf9a53246f791917d0148cee1f7840d736109b
Shivani Sharma <shivanisharma1941995@gmail.com>
2025-01-30 13:49:09 +0530

    added modular css

--- pages/static/users/index.jsx
+++ pages/static/users/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
  // @ts-ignore
+ import Style from "../../../styles/Users.module.css"
  const Users = (props) => {
 
     return (
@@ -7,7 +8,7 @@
       <h1>This is user home page</h1>
       <h4> Users </h4>
       {  // @ts-ignore
-       props.data.users.map((user) => <li key={user.id}>{ user.firstName}</li>)}
+       props.data.users.map((user) => <li className={Style.text} key={user.id}>{ user.firstName}</li>)}
       </>
     )
   
